e7186c2f27bc279d52ff620387eb833a264a8356
qiansheng91 <qiansheng.zx@gmail.com>
2023-03-16 00:30:14 +0800

    Change user-full-name and add python debugger

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "QianSheng"
+      user-mail-address "qiansheng.zx@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -74,3 +74,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(after! dap-mode
+  (setq dap-python-debugger 'debugpy))
